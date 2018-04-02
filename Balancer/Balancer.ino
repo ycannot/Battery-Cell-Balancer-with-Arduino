bdde545d245a869fb8f8e52508ae444a4fced169
ycannot <yigit.yilmaz@std.antalya.edu.tr>
2018-04-02 21:04:00 +0300

    commit #4

--- Balancer/Balancer.ino
+++ Balancer/Balancer.ino
@@ -1,7 +1,7 @@
-//variate inputNumber to increase or decrease # of inputs
-//variate analog_inputs and digital_outputs to increase, decrease # of pins or to change pins
+//variate inputNumber to increase or decrease number of inputs
+//variate analog_inputs and digital_outputs to increase, decrease number of pins or to change pins
 float voltage_div=3,inputNumber=4, temporary, average;
-int analog_inputs[]={A0,A1,A2,A3};
+byte analog_inputs[]={A0,A1,A2,A3};
 int digital_outputs[]={2,3,4,5};
 float voltages[]={};
 
@@ -15,27 +15,31 @@ void blinking(){
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   
   for (int i=0; i<inputNumber; i++){
     //pinMode(analog_inputs[i], INPUT);
     pinMode(digital_outputs[i], OUTPUT);
-    voltages[i]=0;
+    //voltages[i]=0;
   }
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print("hello");
+  //Serial.print("hello");
   temporary=0;
   for (int i=0; i<inputNumber; i++){
     voltages[i] = (analogRead(analog_inputs[i])*voltage_div*5/1023)-temporary;
     temporary += voltages[i];
+    Serial.println(analog_inputs[i]);    
+    Serial.print("pin: ");
+    Serial.println(voltages[i]);
+    Serial.print("\n"); 
   }
   average = temporary/inputNumber;
-  Serial.println(average);
+  //Serial.println(average);
 
   for (int i=0; i<inputNumber; i++){
     if (voltages[i] > average){
